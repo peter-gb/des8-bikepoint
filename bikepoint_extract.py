abc99ab98080c680697268ca59d9af26448633eb
peter-gb <43183973+peter-gb@users.noreply.github.com>
2026-09-25 11:09:54 +0200

    Added logging to the script

--- bikepoint_extract.py
+++ bikepoint_extract.py
@@ -5,6 +5,7 @@ import json
 import os
 from datetime import datetime
 import time
+import logging
 
 #define our url to get the data from the TFL API
 url = "https://api.tfl.gov.uk/BikePoint"
@@ -25,11 +26,29 @@ max_retry = 5
 attempt = 0
 delay = 10
 
+# prepare logging
+# create folder for logging if it doesn't exist, prepare log filename with timestamp
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+log_filename = f'{log_dir}/extract{timestamp}.log'
+
+# configure logging file and messages
+logging.basicConfig(
+    filename=log_filename,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger()
+logger.info('Logger successfully initialised')
+
+#make the API calls, inside a loop that will retry the request if it fails, up to a maximum number of attempts
 while attempt < max_retry:
 
     # make a request to the TFL API and get the response
     response = requests.get(url)
     print(response.status_code)
+    logger.info(f'API request made. Response status code: {response.status_code}')
 
     # save the response data to a JSON file
     data = response.json()
@@ -40,14 +59,39 @@ while attempt < max_retry:
     # do some error handling based on the status code of the response
     status = response.status_code
 
+# on success
     if 200 <= status < 300:
-        print(f'File {filename} was successfully saved')
-        break
+
+        # check if the data is empty and print a warning message if it is
+        if len(data) > 0:
+            try:
+                with open(filename, 'w') as file:
+                    json.dump(data, file)
+                    print(f'File {filename} was successfully saved')
+                    logger.info(f'File {filename} was successfully saved')
+
+
+            except Exception as e:
+                print(f'An error occurred: {e}')
+                logger.error(f'An error occurred while saving the file: {e}')
+            break
+        else:
+            print(f'Warning: The data is empty. Check the API response.')
+            logger.warning(f'Warning: The data is empty. Check the API response.')
+            break
+
+# on error, retry after a delay
     elif status < 200 or status >= 500:
-        print(f'Status code {status}')
-        break   
+       
+        # wait for a delay before retrying
+        time.sleep(delay)
+        attempt += 1
+        print(f'Status code {status}. Retrying. This was attempt {attempt}')
+        logger.warning(f'Status code {status}. Retrying. This was attempt {attempt}')
+
+# unknown error, break the loop and print an error message
     else:
         print(f'Error - Status code{status}. Fix it!!!')
-        attempt += 1
+        logger.error(f'Error - Status code{status}. Fix it!!!')
         break
 
